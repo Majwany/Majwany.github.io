7532bbc0b5f803aaa7aa4879b5890178f8cc9ed9
Majwany <johnson99kamau@gmail.com>
2025-04-17 09:41:37 +0300

    “Add Land Area Calculator project”.

---
+++ calculate_area.py
@@ -0,0 +1,7 @@
+# Calculate the area of a rectangular plot
+length = float(input("Enter the length of the plot (in meters): "))
+width = float(input("Enter the width of the plot (in meters): "))
+
+area = length * width
+
+print(f"The area of the plot is {area} square meters.")
